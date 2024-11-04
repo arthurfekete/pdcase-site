6ef91e7f1b8a120eef38164f2f18ee5ff296ac07
Felipe <felipe.deoliveira@pdcase.com.br>
2024-11-04 15:52:30 -0300

    Personalização da sessão 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { Session1Component } from './components/session-1/session-1.component';
 import { Session2Component } from './components/session-2/session-2.component';
+import { Session3Component } from './components/session-3/session-3.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, Session1Component,FooterComponent, Session2Component],
+  imports: [RouterOutlet, HeaderComponent, Session1Component,FooterComponent, Session2Component, Session3Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
