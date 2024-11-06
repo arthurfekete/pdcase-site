db33a1ff27648d4afa0464c16953bca0751fb3ea
Felipe <felipe.deoliveira@pdcase.com.br>
2024-11-06 14:48:03 -0300

    Criação das páginas da header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainComponentComponent } from './components/maincomponent/maincomponent.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { CareerComponent } from './pages/career/career.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
 
 export const routes: Routes = [
+    { path: '', component: MainComponentComponent }, 
+    { path: 'about', component: AboutComponent, },
+    { path: 'services', component: ServicesComponent, },
+    { path: 'career', component: CareerComponent, },
+    { path: 'contact', component: ContactComponent, },
+    
 ];
