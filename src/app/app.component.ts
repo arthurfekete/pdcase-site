60b76f8c8eff44c9755f8394f86bdfc4549e2520
Arthur Fekete <arthur.othero@pdcase.com.br>
2024-11-06 13:45:25 -0300

    Animação da sessão 2 e alinhamento dos elementos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+//Componentes
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { Session1Component } from './components/session-1/session-1.component';
