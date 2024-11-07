819863b9e5921d509e02e302973a2d9e8ec716eb
Arthur Fekete <arthur.othero@pdcase.com.br>
2024-11-07 13:17:20 -0300

    Mudança no tamanho das fontes

---
+++ src/app/components/main-component/main-component.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Session1Component } from '../session-1/session-1.component';
+import { Session2Component } from '../session-2/session-2.component';
+import { Session3Component } from '../session-3/session-3.component';
+import { Session4Component } from '../session-4/session-4.component';
+
+@Component({
+  selector: 'app-main-component',
+  standalone: true,
+  imports: [Session1Component, Session2Component, Session3Component, Session4Component],
+  templateUrl: './main-component.component.html',
+  styleUrl: './main-component.component.css'
+})
+export class MainComponentComponent {
+
+}
