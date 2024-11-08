ad0e9451287a23b564746f534cde3e0fb97119ac
kaue19santos <kaue10166@gmail.com>
2024-11-08 12:04:47 -0300

    update services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 //Componentes
 import { HeaderComponent } from "./components/header/header.component";
@@ -11,15 +11,25 @@ import { Session2Component } from './components/session-2/session-2.component';
 import { Session3Component } from './components/session-3/session-3.component';
 import { Session4Component } from './components/session-4/session-4.component';
 import { AboutComponent } from './pages/about/about.component';
+import { OtherHeaderComponent } from "./pages/other-header/other-header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, Session1Component,FooterComponent, Session2Component, Session3Component, Session4Component, AboutComponent],
+  imports: [RouterOutlet, HeaderComponent, Session1Component, FooterComponent, Session2Component, Session3Component, Session4Component, AboutComponent, OtherHeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pdcase-site';
-  
+  isHomePage: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.isHomePage = this.router.url === '/'; // Verifique se está na página principal
+    });
+  }
 }
