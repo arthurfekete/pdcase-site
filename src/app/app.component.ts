26309498cbc4b0da7aa57cd29470a782c7583319
kaue19santos <kaue10166@gmail.com>
2024-11-08 12:24:02 -0300

    updates cabeçalho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,17 @@ import { Session2Component } from './components/session-2/session-2.component';
 import { Session3Component } from './components/session-3/session-3.component';
 import { Session4Component } from './components/session-4/session-4.component';
 import { AboutComponent } from './pages/about/about.component';
-import { OtherHeaderComponent } from "./pages/other-header/other-header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, Session1Component, FooterComponent, Session2Component, Session3Component, Session4Component, AboutComponent, OtherHeaderComponent, CommonModule],
+  imports: [RouterOutlet, HeaderComponent, Session1Component, FooterComponent, Session2Component, Session3Component, Session4Component, AboutComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+title = 'pdcase-site';
+
   isHomePage: boolean = false;
 
   constructor(private router: Router) {}
