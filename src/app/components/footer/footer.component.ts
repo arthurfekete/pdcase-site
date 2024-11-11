0d0c8f121406103945a5684acbf897c72258f350
Arthur Fekete <arthur.othero@pdcase.com.br>
2024-11-11 15:35:47 -0300

    teste

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CoreBancarioComponent } from '../../pages/core-bancario/core-bancario.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CoreBancarioComponent],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
