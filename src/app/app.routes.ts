304e6b48049085b586a57c2fd0ec4bb9d385b578
Arthur Fekete <arthur.othero@pdcase.com.br>
2024-11-11 15:32:40 -0300

    Criação do core bancário e mudança das fontes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { CareerComponent } from './pages/career/career.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { CoreBancarioComponent } from './pages/core-bancario/core-bancario.component';
 
 
 export const routes: Routes = [
@@ -12,5 +13,6 @@ export const routes: Routes = [
     { path: 'services', component: ServicesComponent, },
     { path: 'career', component: CareerComponent, },
     { path: 'contact', component: ContactComponent, },
+    { path: 'core-bancario', component: CoreBancarioComponent, },
     
 ];
