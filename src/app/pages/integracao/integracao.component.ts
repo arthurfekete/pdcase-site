27c71e24a12fd538f09e55b7e6275aafe32b6c26
kaue19santos <kaue10166@gmail.com>
2024-11-18 10:21:14 -0300

    finalizei as páginas que faltavam do footer e iniciei a página MPS.BR que fica dentro da página Sobre

---
+++ src/app/pages/integracao/integracao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-integracao',
+  standalone: true,
+  imports: [],
+  templateUrl: './integracao.component.html',
+  styleUrl: './integracao.component.css'
+})
+export class IntegracaoComponent {
+
+}
