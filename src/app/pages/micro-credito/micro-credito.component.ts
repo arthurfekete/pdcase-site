72bd417483039962ee384c32f919d75094b2277c
Arthur Fekete <arthur.othero@pdcase.com.br>
2024-11-21 14:53:57 -0300

    Criação das 4 páginas (daily)

---
+++ src/app/pages/micro-credito/micro-credito.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-micro-credito',
+  standalone: true,
+  imports: [],
+  templateUrl: './micro-credito.component.html',
+  styleUrl: './micro-credito.component.css'
+})
+export class MicroCreditoComponent {
+
+}
