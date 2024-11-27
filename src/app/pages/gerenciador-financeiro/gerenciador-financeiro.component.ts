c38acd08fafa81797194fd102b61361d9000ec35
kaue19santos <kaue10166@gmail.com>
2024-11-27 10:55:45 -0300

    agências de fomento concluída e fiz a pagina gerenciador financeiro de Mercado digital

---
+++ src/app/pages/gerenciador-financeiro/gerenciador-financeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gerenciador-financeiro',
+  standalone: true,
+  imports: [],
+  templateUrl: './gerenciador-financeiro.component.html',
+  styleUrl: './gerenciador-financeiro.component.css'
+})
+export class GerenciadorFinanceiroComponent {
+
+}
---
+++ src/app/pages/sped/sped.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sped',
+  standalone: true,
+  imports: [],
+  templateUrl: './sped.component.html',
+  styleUrl: './sped.component.css'
+})
+export class SpedComponent {
+
+}
