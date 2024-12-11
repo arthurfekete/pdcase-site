5b6d74d32dfea0e42355a2a100a623b697229fcd
kaue19santos <kaue10166@gmail.com>
2024-12-11 13:15:29 -0300

    arrumei a animação nas barrinhas que ficam embaixo dos links no cabeçalho

--- src/app/components/services-header/services-header.component.ts
+++ src/app/components/services-header/services-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-services-header',
@@ -8,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './services-header.component.css'
 })
 export class ServicesHeaderComponent {
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
+    const logo = document.getElementById('logo') as HTMLImageElement; // Garantir que é um elemento <img>
+    const header = document.getElementById('header');
+    const links = document.getElementsByClassName('links') as HTMLCollectionOf<HTMLElement>;
+    
+    if (header) {
+      if(scrollTop > 30) {
+        for (let i = 0; i < links.length; i++) {
+          links[i].style.marginTop = '-10px'; 
+        }
+        header.style.backgroundColor = '#161D26';
+        header.style.height = '96px';
+        logo.src = '/logo-deitada.svg';
+        logo.style.width = '154px';
+        logo.style.height = '48px';
+      } else {
+        for (let i = 0; i < links.length; i++) {
+          links[i].style.marginTop = '-30px'; 
+        }
+        header.style.backgroundColor = 'transparent';
+        logo.src = 'logo.svg';
+        logo.style.width = '72px';
+        logo.style.height = '72px';
+      }
+    }
+  }
 }
