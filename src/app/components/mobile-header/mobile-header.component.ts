e5bb79fef9fa06b3cb08f76b66d18a3cb5faea2e
kaue19santos <kaue10166@gmail.com>
2024-12-19 11:57:28 -0300

    trabalhando no cabeçalho

--- src/app/components/mobile-header/mobile-header.component.ts
+++ src/app/components/mobile-header/mobile-header.component.ts
@@ -20,31 +20,19 @@ export class MobileHeaderComponent {
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
     const header = document.getElementById('header');
-    const mobileNav = document.getElementById('mobile-nav');
-    const navElement = document.querySelector('.nav') as HTMLDivElement | null;
+    const container = document.getElementById('container');
 
     const iconeMenu = document.querySelector('.icone-menu') as HTMLElement;
+
     if (header) {
       if (scrollTop > 3) {
         header.style.backgroundColor = '#161D26';
-        header.style.height = '60px';
-        mobileNav!.style.height = '65px';
-        mobileNav!.style.marginTop = '0px';
-        if (this.isMainPage) {
-          mobileNav!.style.marginTop = '5px';
-          iconeMenu.style.marginTop = '-18px';
-        }
+
       } else {
         if (scrollTop > 30) {
           header.style.backgroundColor = '#161D26';
-          header.style.height = '60px';
-          mobileNav!.style.height = '65px';
-          mobileNav!.style.marginTop = '5px';
         } else {
           header.style.backgroundColor = 'transparent';
-          mobileNav!.style.padding = '20px';
-          mobileNav!.style.margin = 'auto';
-          mobileNav!.style.height = '65px';
         }
       }
     }
